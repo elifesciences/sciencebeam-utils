196c2b12926be99d45614b2bc0623698c84c8814
Daniel Ecer <de-code@users.noreply.github.com>
2019-01-08 11:04:32 +0000

    show examples of missing files

--- sciencebeam_utils/tools/check_file_list_test.py
+++ sciencebeam_utils/tools/check_file_list_test.py
@@ -5,6 +5,7 @@ import pytest
 import sciencebeam_utils.tools.check_file_list as check_file_list_module
 from sciencebeam_utils.tools.check_file_list import (
     map_file_list_to_file_exists,
+    format_file_list,
     format_file_exists_results,
     check_files_and_report_result
 )
@@ -24,23 +25,33 @@ class TestMapFileListToFileExists(object):
             FileSystems.exists.assert_called_with(FILE_1)
 
 
+class TestFormatFileList(object):
+    def test_should_format_multiple_files(self):
+        assert (
+            format_file_list([FILE_1, FILE_2]) ==
+            "['%s', '%s']" % (FILE_1, FILE_2)
+        )
+
+
 class TestFormatFileExistsResults(object):
     def test_should_format_no_files(self):
         assert (
-            format_file_exists_results([]) ==
+            format_file_exists_results([], []) ==
             'empty file list'
         )
 
     def test_should_format_all_files_exist(self):
         assert (
-            format_file_exists_results([True, True]) ==
+            format_file_exists_results([True, True], [FILE_1, FILE_2]) ==
             'files exist: 2 (100%), files missing: 0 (0%)'
         )
 
     def test_should_format_files_partially_exist(self):
         assert (
-            format_file_exists_results([True, False]) ==
-            'files exist: 1 (50%), files missing: 1 (50%)'
+            format_file_exists_results([True, False], [FILE_1, FILE_2]) ==
+            'files exist: 1 (50%), files missing: 1 (50%) (example missing: {})'.format(
+                format_file_list([FILE_2])
+            )
         )
 
 
@@ -52,7 +63,7 @@ class TestCheckFileListAndReportResults(object):
                 map_file_list_to_file_exists_mock.return_value = [True, True]
                 check_files_and_report_result([FILE_1, FILE_2])
                 map_file_list_to_file_exists_mock.assert_called_with([FILE_1, FILE_2])
-                format_file_exists_results_mock.assert_called_with([True, True])
+                format_file_exists_results_mock.assert_called_with([True, True], [FILE_1, FILE_2])
 
     def test_should_raise_error_if_none_of_the_files_were_found(self):
         m = check_file_list_module
