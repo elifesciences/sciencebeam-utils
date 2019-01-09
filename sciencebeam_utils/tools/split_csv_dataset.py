9dd26c944797b874299030e511313e712aae9aa2
Daniel Ecer <de-code@users.noreply.github.com>
2019-01-09 21:05:59 +0000

    removed no longer if statement in _to_hashable

--- sciencebeam_utils/tools/split_csv_dataset.py
+++ sciencebeam_utils/tools/split_csv_dataset.py
@@ -74,7 +74,7 @@ def split_row_chunks(rows, chunk_size_list):
 
 
 def _to_hashable(value):
-    return tuple(value) if isinstance(value, list) else value
+    return tuple(value)
 
 
 def _to_rows_set(rows):
