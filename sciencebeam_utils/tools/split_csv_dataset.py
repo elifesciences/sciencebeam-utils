ff9e3585ffd66cb848d8aa96b06b388446aab56f
Daniel Ecer <de-code@users.noreply.github.com>
2019-01-09 20:52:45 +0000

    fixed return type

--- sciencebeam_utils/tools/split_csv_dataset.py
+++ sciencebeam_utils/tools/split_csv_dataset.py
@@ -95,7 +95,7 @@ def split_rows(
         percentages,  # type: List[float]
         fill=False,  # type: bool
         existing_split=None):  # type: List[List[Row]]
-    # type: (...) -> List[Row]
+    # type: (...) -> List[List[Row]]
     if not existing_split:
         return _split_rows_without_existing_split(rows, percentages, fill=fill)
     LOGGER.debug('existing_split: %s', existing_split)
