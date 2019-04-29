d75982bc66d4acd409e16607980c76eaf20c1d00
Daniel Ecer <de-code@users.noreply.github.com>
2019-04-29 09:37:01 +0100

    only enable debug logging for sciencebeam_utils (when running tests)

--- sciencebeam_utils/conftest.py
+++ sciencebeam_utils/conftest.py
@@ -6,4 +6,5 @@ import pytest
 @pytest.fixture(scope='session', autouse=True)
 def setup_logging():
     logging.root.handlers = []
-    logging.basicConfig(level='DEBUG')
+    logging.basicConfig(level='INFO')
+    logging.getLogger('sciencebeam_utils').setLevel('DEBUG')
