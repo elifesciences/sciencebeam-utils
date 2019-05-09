c7298e692a4910e039ec8fa0f2e5bf3769aae088
Daniel Ecer <de-code@users.noreply.github.com>
2019-05-09 15:00:10 +0100

    fixed reading README

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as f:
     REQUIRED_PACKAGES = f.readlines()
 
 
-with open('README.md', encoding='utf-8') as f:
+with open('README.md', 'r') as f:
     long_description = f.read()
 
 
