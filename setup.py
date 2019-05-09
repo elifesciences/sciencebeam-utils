3539bc125ba69f6ce43b62d84bac0d98eeea9ab2
Daniel Ecer <de-code@users.noreply.github.com>
2019-05-09 14:29:53 +0100

    added author and url

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ packages = find_packages()
 setup(
     name='sciencebeam_utils',
     version=sciencebeam_utils.__version__,
+    author="eLife Sciences Publications, Ltd",
+    url="https://github.com/elifesciences/sciencebeam-utils",
     install_requires=REQUIRED_PACKAGES,
     packages=packages,
     include_package_data=True,
