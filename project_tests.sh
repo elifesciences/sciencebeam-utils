181ed6ae9bb3b4892943274dadf92db8305e4574
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-02 13:04:43 +0100

    reversed order in which tests are run

--- project_tests.sh
+++ project_tests.sh
@@ -5,14 +5,14 @@ set -e
 # avoid issues with .pyc/pyo files when mounting source directory
 export PYTHONOPTIMIZE=
 
-echo "running tests"
-pytest -p no:cacheprovider
+echo "running flake8"
+flake8 sciencebeam_utils tests
 
 echo "running pylint"
 PYLINTHOME=/tmp/sciencebeam-utils \
   pylint sciencebeam_utils tests
 
-echo "running flake8"
-flake8 sciencebeam_utils tests
+echo "running tests"
+pytest -p no:cacheprovider
 
 echo "done"
