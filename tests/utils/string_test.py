3c18ef96b9a43d64bacc40d25d67dc07dcbeacb9
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-21 17:21:12 +0100

    added parse_list

---
+++ tests/utils/string_test.py
@@ -0,0 +1,17 @@
+from sciencebeam_utils.utils.string import (
+    parse_list
+)
+
+
+class TestParseList(object):
+    def test_should_return_empty_list_for_empty_string(self):
+        assert parse_list('') == []
+
+    def test_should_return_single_item(self):
+        assert parse_list('abc') == ['abc']
+
+    def test_should_return_multiple_items(self):
+        assert parse_list('abc,def') == ['abc', 'def']
+
+    def test_should_ignore_space(self):
+        assert parse_list(' abc , def ') == ['abc', 'def']
