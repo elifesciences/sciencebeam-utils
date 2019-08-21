e0b3867b83672d52219bcbcfee61f1af85f2c6c3
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-21 17:35:09 +0100

    also accept --num-workers and --job-name (#57)
    * add_cloud_args now returns parser
    
    * also accept --num-workers and --job-name

--- sciencebeam_utils/beam_utils/main.py
+++ sciencebeam_utils/beam_utils/main.py
@@ -72,19 +72,21 @@ def add_cloud_args(parser):
         help='The cloud project name to be used for running this pipeline'
     )
     parser.add_argument(
-        '--num_workers',
+        '--num_workers', '--num-workers',
         default=1,
         type=int,
         help='The number of workers.'
     )
     parser.add_argument(
-        '--job_name', type=str, required=False,
+        '--job_name', '--job-name',
+        type=str, required=False,
         help='The name of the cloud job'
     )
     parser.add_argument(
         '--job-name-suffix', type=str, required=False,
         help='A suffix appended to the job name'
     )
+    return parser
 
 
 def process_cloud_args(parsed_args, output_path, name=None):
