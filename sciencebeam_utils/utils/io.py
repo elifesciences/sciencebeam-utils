5c1c9f4a2c273da0876f7315241e8b0c7bef062b
medecau <medecau@gmail.com>
2021-09-13 10:23:19 +0200

    Fix arg name (#178)
    PLEASE, for the love of all good things DO NOT disable warnings OR if you do PLEASE understand what you are doing!
    
    Here's the docs: https://docs.python.org/3/library/os.html#os.makedirs

--- sciencebeam_utils/utils/io.py
+++ sciencebeam_utils/utils/io.py
@@ -2,16 +2,16 @@ import os
 import errno
 
 
-def makedirs(path, exists_ok=False):
+def makedirs(path, exist_ok=False):
     try:
         # Python 3
-        os.makedirs(path, exists_ok=exists_ok)  # pylint: disable=unexpected-keyword-arg
+        os.makedirs(path, exist_ok=exist_ok)
     except TypeError:
         # Python 2
         try:
             os.makedirs(path)
         except OSError as e:
-            if e.errno == errno.EEXIST and os.path.isdir(path) and exists_ok:
+            if e.errno == errno.EEXIST and os.path.isdir(path) and exist_ok:
                 pass
             else:
                 raise
