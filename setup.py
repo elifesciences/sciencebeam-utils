477623c4439d18b7e4abbca91c5c424a7fae44cd
Daniel Ecer <de-code@users.noreply.github.com>
2021-10-15 11:09:15 +0100

    specified encoding

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 import sciencebeam_utils
 
 
-with open('requirements.txt', 'r') as f:
+with open('requirements.txt', 'r', encoding='utf-8') as f:
     REQUIRED_PACKAGES = f.readlines()
 
 
-with open('README.md', 'r') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 
